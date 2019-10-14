ab14d34758e3b02c90f84d3d97a3943625ed08bb
Aizat Faiz <aizat.faiz@gmail.com>
2019-10-14 12:28:18 +0800

    fish: include raspbian installation path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 test -e /usr/local/Cellar/fzf/0.18.0/shell/key-bindings.fish ; and source /usr/local/Cellar/fzf/0.18.0/shell/key-bindings.fish
+test -e /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish ; and source /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish
 
